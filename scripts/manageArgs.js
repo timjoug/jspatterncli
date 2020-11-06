07cc946db030a0d5b608fbce228dd989f6b750b4
timjoug <tjouglet@partner.auchan.fr>
2020-11-06 16:16:04 +0100

    CLI 1.0.6 : parseArgs() rework : no more function call

--- scripts/manageArgs.js
+++ scripts/manageArgs.js
@@ -1,5 +1,3 @@
-const dataHandling = require('./dataHandling')
-
 /**
  * This function will parse the list of arguments in order to use it for the function execution.
  * @param {Array} argumentList - List of the arguments, passed during the app.js execution
